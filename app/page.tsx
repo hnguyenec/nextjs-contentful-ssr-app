2d43d3043b9c71c4964ae4c605e0f334e255eb53
hnguyenec <hnguyenec@gmail.com>
2025-03-03 19:23:58 +1100

    fix draftMode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import { draftMode } from "next/headers";
+import Link from "next/link";
 
-import Date from "./date";
-import CoverImage from "./cover-image";
 import Avatar from "./avatar";
+import CoverImage from "./cover-image";
+import Date from "./date";
 import MoreStories from "./more-stories";
 
 import { getAllPosts } from "@/lib/api";
@@ -77,7 +77,7 @@ function HeroPost({
 }
 
 export default async function Page() {
-  const { isEnabled } = draftMode();
+  const { isEnabled } = await draftMode();
   const allPosts = await getAllPosts(isEnabled);
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
